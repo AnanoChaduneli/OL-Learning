02abde3d38551933f3a66e8e324dc1cbe8676e60
localadmin <localadmin@DESKTOP-LHU2UNM>
2023-02-16 01:32:24 +0400

    Add project files.

---
+++ OL Learning/Program.cs
@@ -0,0 +1,11 @@
+﻿class MyMain
+{
+    static void Main(string[] args) // Entry Point
+    {
+        //Displays Messages in the Console
+        Console.WriteLine("Hello");
+        Console.WriteLine("Mr. Archil");
+    }
+
+}
+
