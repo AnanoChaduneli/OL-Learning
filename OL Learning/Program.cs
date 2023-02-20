00aa7935b333125ae700b5002901f6cc0f9a6c6c
AnanoChaduneli <a_chaduneli@cu.edu.ge>
2023-02-21 00:57:50 +0400

    This is new code for lesson 3

--- OL Learning/Program.cs
+++ OL Learning/Program.cs
@@ -3,9 +3,19 @@
     static void Main(string[] args) // Entry Point
     {
         //Displays Messages in the Console
-        Console.WriteLine("Hello");
-        Console.WriteLine("Mr. Archil");
+        Console.WriteLine();
+        Console.WriteLine("   |   |   ");
+        Console.WriteLine(" 1 | 2 | 3 ");
+        Console.WriteLine("___|___|___");
+        Console.WriteLine("   |   |   ");
+        Console.WriteLine(" 4 | 5 | 6 ");
+        Console.WriteLine("___|___|___");
+        Console.WriteLine("   |   |   ");
+        Console.WriteLine(" 7 | 8 | 9 ");
+        Console.WriteLine("   |   |   ");
+
     }
+
 
 }
 
