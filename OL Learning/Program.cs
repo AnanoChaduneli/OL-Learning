acd3a1c5d121c9943f0a8b6ece802be75942fdc6
AnanoChaduneli <a_chaduneli@cu.edu.ge>
2023-02-24 02:57:08 +0400

    update

--- OL Learning/Program.cs
+++ OL Learning/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Metrics;
+using System.Numerics;
 using System.Runtime.InteropServices;
 
 /* Lesson 4 Homework 1
@@ -17,16 +19,18 @@ using System.Runtime.InteropServices;
  * */
 
 class MyMain
-{      
+{
     static void Main(string[] args) // Entry Point
     {   //Lesson 4 Homework 1
         Console.WriteLine("\nLesson 4 Homework 1\n");
         Console.Write("Sum -  ");
         getParams(5, 10); // პარამეტრის გადაცემა; მეთოდის გამოძახება
+
         //Lesson 4 Homework 2
         getMinMax(); // მეთოდის გამოძახება
-        
 
+        //Lesson 4 Homework 3
+        getFruitName(); // მეთოდის გამოძახება
     }
 
     /// <summary>
@@ -36,7 +40,7 @@ class MyMain
     {
         Console.WriteLine(x + y);
     }
-    
+
     /// <summary>
     /// Lesson 4 Homework 2
     /// </summary>
@@ -44,36 +48,70 @@ class MyMain
     {
 
         Console.WriteLine("\n\nLesson 4 Homework 2\n");
-        Console.Write("Number N1 - ");
+        Console.Write("Please, enter number N1 - ");
         decimal number1 = decimal.Parse(Console.ReadLine());
-        Console.Write("Number N2 - ");
+        Console.Write("Please, enter number N2 - ");
         decimal number2 = decimal.Parse(Console.ReadLine());
-        Console.Write("Number N3 - ");
+        Console.Write("Please, enter number N3 - ");
         decimal number3 = decimal.Parse(Console.ReadLine());
-        Console.Write("Number N4 - ");
+        Console.Write("Please, enter number N4 - ");
         decimal number4 = decimal.Parse(Console.ReadLine());
         Console.WriteLine();
 
         //Max Value
         if (number1 > number2 && number1 > number3 && number1 > number4)
-        {   Console.WriteLine("Max value is - " + number1);
-              } else if (number2 > number3 && number2 > number4)
-          { Console.WriteLine("Max value is - " + number2);
-              } else if (number3 > number4)
-          { Console.WriteLine("Max value is - " + number3);
-              } else
-          { Console.WriteLine("Max value is - " + number4);
+        { Console.WriteLine("Max value is - " + number1);
+        } else if (number2 > number3 && number2 > number4)
+        { Console.WriteLine("Max value is - " + number2);
+        } else if (number3 > number4)
+        { Console.WriteLine("Max value is - " + number3);
+        } else
+        { Console.WriteLine("Max value is - " + number4);
         }
-       
+
         //Min Value
         if (number1 < number2 && number1 < number3 && number1 < number4)
-        {   Console.WriteLine("Min value is - " + number1);
-             } else if (number2 < number3 && number2 < number4)
-          { Console.WriteLine("Min value is - " + number2);
-             } else if (number3 < number4)
-          { Console.WriteLine("Min value is - " + number3);
-             } else
-          { Console.WriteLine("Max value is - " + number4);
+        { Console.WriteLine("Min value is - " + number1);
+        } else if (number2 < number3 && number2 < number4)
+        { Console.WriteLine("Min value is - " + number2);
+        } else if (number3 < number4)
+        { Console.WriteLine("Min value is - " + number3);
+        } else
+        { Console.WriteLine("Max value is - " + number4);
+        }
+    }
+
+    /// <summary>
+    /// Lesson 4 Homework 3
+    /// </summary>
+    static void getFruitName()
+    {
+
+        Console.WriteLine("\n\nLesson 4 Homework 3");
+        Console.WriteLine("\nChoose one of the following fruits - apple, watermelon, melon, cherry, straweberry.");
+        Console.Write("\nPlease, enter one of them: ");
+        string fruit = Console.ReadLine();
+
+        switch (fruit) 
+        {
+            case ("apple"):
+                Console.WriteLine("This is an apple.");
+                break;
+            case ("watermelon"):
+                Console.WriteLine("This is a watermelon.");
+                break;
+            case ("melon"):
+                Console.WriteLine("This is a melon.");
+                break;
+            case ("cherry"):
+                Console.WriteLine("This is a cherry.");
+                break;
+            case ("Straweberry"):
+                Console.WriteLine("This is a straweberry.");
+                break;
+            default:
+                Console.WriteLine("Not found");
+                break;
         }
     }
 }
