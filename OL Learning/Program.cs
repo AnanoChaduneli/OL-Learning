387854d1385c8109d303a0fb5bff42023bcbc2df
AnanoChaduneli <a_chaduneli@cu.edu.ge>
2023-02-24 00:52:33 +0400

    lesson 4 homework 1

--- OL Learning/Program.cs
+++ OL Learning/Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 
 class MyMain
-{
-    static void Main(string[] args) // Entry Point
-    {
-        /*Lesson 4 Homework 1
+{      
+    /*Lesson 4 Homework 1
          * 1. შევქმნათ მეთოდი, რომელსაც გადავცემთ ორ სრულ რიცხვს. მეთოდმა უნდა დააბრუნოს ორი რიცხვის ჯამი. 
          * გამოვიყვანოთ ეს ჯამი შესვლის წერტილის მეთოდის მეშვეობით კონსოლში.
          */
+    static void Main(string[] args) // Entry Point
+    {
         getParams(5, 10, "Sum is - ");
         
     }
@@ -17,7 +17,6 @@ class MyMain
         Console.WriteLine("\nLesson 4 Homework 1\n");
         Console.WriteLine(name +( x + y));
     }
-
 
 }
 
