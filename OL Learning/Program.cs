711d9d912349df62e1f4399c09570c74c0d56965
AnanoChaduneli <a_chaduneli@cu.edu.ge>
2023-02-24 02:23:16 +0400

    UPDATE

--- OL Learning/Program.cs
+++ OL Learning/Program.cs
@@ -52,35 +52,29 @@ class MyMain
         if (number1 > number2 && number1 > number3 && number1 > number4)
         {
             Console.WriteLine("Max value is - " + number1);
-        }
-        else if (number2 > number3 && number2 > number4)
-        {
+              } else if (number2 > number3 && number2 > number4)
+          {
             Console.WriteLine("Max value is - " + number2);
-        }
-        else if (number3 > number4)
-        {
+              } else if (number3 > number4)
+          {
             Console.WriteLine("Max value is - " + number3);
-        }
-        else
-        {
+              } else
+          {
             Console.WriteLine("Max value is - " + number4);
         }
        
         //Min Value
         if (number1 < number2 && number1 < number3 && number1 < number4)
         {
-            Console.WriteLine("Min value is - " + number1);
-        }
-        else if (number2 < number3 && number2 < number4)
-        {
+             Console.WriteLine("Min value is - " + number1);
+             } else if (number2 < number3 && number2 < number4)
+          {
             Console.WriteLine("Min value is - " + number2);
-        }
-        else if (number3 < number4)
-        {
+             } else if (number3 < number4)
+          {
             Console.WriteLine("Min value is - " + number3);
-        }
-        else
-        {
+             } else
+          {
             Console.WriteLine("Max value is - " + number4);
         }
     }
